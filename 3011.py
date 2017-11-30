f91b8f442733887c876a980177259d430a29feaa
1-21-01-22B-03\pc1-21-01-22B-03 <cabuyales.jonathan3infor@gmail.com>
2017-11-30 09:16:17 -0500

    Programa calcular areas

---
+++ 3011.py
@@ -0,0 +1,21 @@
+# Area circulo, cuadrado, triangulo
+
+print("Calcular Areas")
+print("1. Triangulo")
+print("2. Circulo")
+print("3. Cuadrado")
+
+num = int(input("Digite un numero: "))
+if num == 1:
+    base = float(input("Ingrese la base: "))
+    altura = float(input("Ingrese la altura: "))
+    base_altura = base*altura
+    area_triangulo = base_altura/2
+
+elif num == 2:
+
+elif num == 3:
+
+else:
+
+
