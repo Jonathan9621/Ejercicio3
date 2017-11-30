eb3bcff052fe07f496a53b25b624274b03a3872a
Ihoaxcel <32933902+Ihoaxcel@users.noreply.github.com>
2017-11-30 09:39:51 -0500

    Cuadrado

--- 3011.py
+++ 3011.py
@@ -4,12 +4,18 @@ print("Calcular Areas")
 print("1. Triangulo")
 print("2. Circulo")
 print("3. Cuadrado")
-def area_triangulo:
-     base = float(input("Ingrese la base: "))
+
+def area_triangulo():
+    base = float(input("Ingrese la base: "))
     altura = float(input("Ingrese la altura: "))
     base_altura = base*altura
     area_triangulo = base_altura/2
     print("El area del triangulo es: ", area_triangulo)
+
+def area_cuadrado ():
+    lado = float (input("Ingrese el lado: "))
+    base_altura = lado * lado
+    print ("El area del cuadrado es: ", area_cuadrado)
 
 num = int(input("Digite un numero: "))
 if num == 1:
@@ -18,7 +24,7 @@ if num == 1:
 elif num == 2:
 
 elif num == 3:
-
+    area_cuadrado()
 else:
 
 
