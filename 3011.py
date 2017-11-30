f62ebc92b5f6d28eab0083904fb6b7464a690d2e
1-21-01-22B-02\pc1-21-01-22B-03 <Darwin19970420@hotmail.com>
2017-11-30 09:33:07 -0500

    area del circulo

--- 3011.py
+++ 3011.py
@@ -4,7 +4,13 @@ print("Calcular Areas")
 print("1. Triangulo")
 print("2. Circulo")
 print("3. Cuadrado")
-def area_triangulo:
+
+def areaCirculo():
+    radio = float(input("Ingrese el radio del circulo"))
+    area = 3.1416*radio
+    return area
+    
+def area_triangulo():
      base = float(input("Ingrese la base: "))
     altura = float(input("Ingrese la altura: "))
     base_altura = base*altura
@@ -12,10 +18,12 @@ def area_triangulo:
     print("El area del triangulo es: ", area_triangulo)
 
 num = int(input("Digite un numero: "))
+
 if num == 1:
    area_triangulo()
 
 elif num == 2:
+    area = areaCirculo()
 
 elif num == 3:
 
